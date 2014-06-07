7e9151e957762c9acdf43d45ef3bfa541696ff08
Otto Giron <ottog2486@gmail.com>
2014-06-07 13:21:20 -0600

    First commit, implemented some simple mongo/jcr tests

---
+++ jcr-oak-rpc-services/src/main/java/org/jumlabs/jcr/oak/rpc/api/AppConfiguration.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.jumlabs.jcr.oak.rpc.api;
+
+import com.mongodb.MongoClientURI;
+import java.net.UnknownHostException;
+import org.apache.jackrabbit.oak.Oak;
+import org.apache.jackrabbit.oak.api.ContentRepository;
+import org.apache.jackrabbit.oak.plugins.document.DocumentMK;
+import org.slf4j.Logger;
+import org.apache.jackrabbit.oak.plugins.document.util.MongoConnection;
+import org.apache.jackrabbit.oak.plugins.index.property.PropertyIndexProvider;
+import org.apache.jackrabbit.oak.plugins.name.NameValidatorProvider;
+import org.apache.jackrabbit.oak.plugins.nodetype.write.InitialContent;
+import org.apache.jackrabbit.oak.security.SecurityProviderImpl;
+import org.apache.jackrabbit.oak.spi.commit.DefaultEditor;
+import org.apache.jackrabbit.oak.spi.state.NodeStore;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ *
+ * @author otto
+ */
+@Configuration
+public class AppConfiguration {
+    private  static final Logger logger = LoggerFactory.getLogger(AppConfiguration.class);
+    
+    
+    public MongoConnection mongoConnection(){
+        MongoClientURI uri = new MongoClientURI("mongodb://localhost/MongoMKDB");
+        MongoConnection mongo = null;
+        try {
+            mongo = new MongoConnection(uri.getURI());
+        } catch (UnknownHostException ex) {
+          logger.error(ex.getMessage(),ex);
+        }
+        return mongo;
+    }
+    
+    
+    
+    public NodeStore nodeStore(){
+        NodeStore store;
+        store = new DocumentMK.Builder().
+                 setMongoDB(mongoConnection().getDB())
+                 .getNodeStore();  
+        return store;
+    }
+    
+    @Bean
+    public ContentRepository contentRepository(){
+        ContentRepository repository;
+        repository = new Oak(nodeStore())
+                .with(new InitialContent())        // add initial content
+                .with(new DefaultEditor())     // automatically set default types
+                .with(new NameValidatorProvider()) // allow only valid JCR names
+                .with(new SecurityProviderImpl())  // use the default security    
+                .with(new PropertyIndexProvider()) // search support for the indexes                
+                .createContentRepository();
+        return repository;
+    }
+    
+}
---
+++ jcr-oak-rpc-services/src/main/java/org/jumlabs/jcr/oak/rpc/api/TestService.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.jumlabs.jcr.oak.rpc.api;
+
+/**
+ *
+ * @author otto
+ */
+public interface TestService {
+    void sayHello();
+}
